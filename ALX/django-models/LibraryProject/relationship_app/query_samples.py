133833c80ac093effad963b9f1195b0ed276a310
Prince Isaac Ofori <pknox03@gmail.com>
2025-08-04 21:03:40 +0000

    Add relationship app

--- ALX/django-models/LibraryProject/relationship_app/query_samples.py
+++ ALX/django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,25 +1,14 @@
-# relationship_app/query_samples.py
-import os
-import django
+from relationship_app.models import Author, Library, Librarian
+# List all books by a specific author
+Author.objects.get(name=author_name)
+Books.objects.filter(author=author)
 
-# Setup
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
-django.setup()
 
-from relationship_app.models import Author, Book, Library, Librarian
+# List all books in a library 
+Library.objects.get(name=library_name)
+library.books.all()
 
-# Query 1: All books by a specific author
-author_name = "Chinua Achebe"
-author = Author.objects.get(name=author_name)
-books_by_author = Book.objects.filter(author=author)
-print(f"Books by {author_name}: {[book.title for book in books_by_author]}")
 
-# Query 2: All books in a library
-library_name = "Central Library"
-library = Library.objects.get(name=library_name)
-books_in_library = library.books.all()
-print(f"Books in {library_name}: {[book.title for book in books_in_library]}")
-
-# Query 3: Retrieve the librarian for a library
-librarian = Librarian.objects.get(library=library)
-print(f"Librarian at {library.name}: {librarian.name}")
+# Retrieve the librarian for a library 
+library = Library.objects.get(name=library_name) 
+Librarian.objects.get(library=library)
