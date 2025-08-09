4a80ac8d91308a820f869d0c2d64e45d06b6e725
Prince Isaac Ofori <pknox03@gmail.com>
2025-08-09 15:07:43 +0000

    refactor: Use explicit view imports in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,25 @@
 from django.urls import path
-from . import views
+from .views import (
+    list_books,
+    LibraryDetailView,
+    CustomLoginView,
+    CustomLogoutView,
+    register,
+    add_book,
+    edit_book,
+    delete_book,
+)
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('register/', views.register, name='register'),
-    # path('admin/', views.admin_view, name='admin_view'),
-    # path('librarian/', views.librarian_view, name='librarian_view'),
-    # path('member/', views.member_view, name='member_view'),
-    path('add_book/', views.add_book, name='add_book'),
-    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
-    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+    # path('admin/', admin_view, name='admin_view'),
+    # path('librarian/', librarian_view, name='librarian_view'),
+    # path('member/', member_view, name='member_view'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
