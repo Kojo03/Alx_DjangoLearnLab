02636f7080eee5f722b4b0bd74c798d345f40944
Prince Isaac Ofori <pknox03@gmail.com>
2025-08-09 16:44:07 +0000

    Clean up .gitignore and commit new changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponseForbidden
-# from .models import UserProfile  # Commented out: UserProfile model not found
+from .models import UserProfile
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.views import LoginView, LogoutView
@@ -36,30 +36,29 @@ class CustomLoginView(LoginView):
 class CustomLogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
 
-# Commented out: UserProfile model not found
-# def is_admin(user):
-#     return user.userprofile.role == 'Admin'
-#
-# def is_librarian(user):
-#     return user.userprofile.role == 'Librarian'
-#
-# def is_member(user):
-#     return user.userprofile.role == 'Member'
-#
-# @login_required
-# @user_passes_test(is_admin)
-# def admin_view(request):
-#     return render(request, 'relationship_app/admin_view.html')
-#
-# @login_required
-# @user_passes_test(is_librarian)
-# def librarian_view(request):
-#     return render(request, 'relationship_app/librarian_view.html'
-#
-# @login_required
-# @user_passes_test(is_member)
-# def member_view(request):
-#     return render(request, 'relationship_app/member_view.html')
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
