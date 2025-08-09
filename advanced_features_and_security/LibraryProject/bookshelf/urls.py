85d13f146a3de9470d849fcbee17bdd862ca6a68
Prince Isaac Ofori <pknox03@gmail.com>
2025-08-09 20:57:43 +0000

    Add views and URLs for bookshelf app

---
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
+    path('exception/', views.raise_exception, name='raise_exception'),
+    path('books_placeholder/', views.books, name='books'),
+]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Book
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
 
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+def raise_exception(request):
+    raise PermissionDenied
+
+def books(request):
+    return HttpResponse("This is the books view.")
