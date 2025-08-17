7d60ce3446aa078481c74ea50319d3c0804569c5
Prince Isaac Ofori <pknox03@gmail.com>
2025-08-17 09:10:33 +0000

    Fix: Add trailing comma to urlpatterns in blog/urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
 
     # Comment CRUD
     path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment-create"),
-    path("post/<int:post_pk>/comments/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
-    path("post/<int:post_pk>/comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
