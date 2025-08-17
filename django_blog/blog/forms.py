5c95e8fb5b74e9d37e3e0ea3e0f99d343402de8f
Prince Isaac Ofori <pknox03@gmail.com>
2025-08-17 09:22:29 +0000

    feat: Add tags field to PostForm

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Post, Comment
+from taggit.forms import TagWidget
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -37,7 +38,10 @@ class PostForm(forms.ModelForm):
     """Form to create and update blog posts"""
     class Meta:
         model = Post
-        fields = ["title", "content"]
+        fields = ["title", "content", "tags"]
+        widgets = {
+            "tags": TagWidget(),
+        }
 
 
 class CommentForm(forms.ModelForm):
