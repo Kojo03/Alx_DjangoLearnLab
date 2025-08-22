a095c4eaddf05d627cf14148245adb7701a51700
Prince Isaac Ofori <pknox03@gmail.com>
2025-08-22 14:04:53 +0000

    refactor: use get_user_model in serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,22 +1,21 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework.authtoken.models import Token
-from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ["id", "username", "email", "bio", "profile_picture", "followers"]
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ["username", "email", "password"]
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"],
